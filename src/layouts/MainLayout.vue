1e1433a67a7d1b236946da15f64f487748fcac2b
lapinRagnar <lapinragnar@gmail.com>
2023-01-04 21:31:47 +0100

    left drawer - activer fontawsome - et link to=...

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,8 +12,55 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+      width="200"
+    >
+      <!-- left drawer content -->
+      <q-icon
+        class="q-pa-md"
+        name="fa-solid fa-dove"
+        size="lg"
+        color="green-4"
+      />
+
+      <q-list>
+        <q-item
+          clickable
+          v-ripple
+          to="/"
+        >
+          <q-item-section avatar>
+            <q-icon
+              color="green-4"
+              name="home"
+              size="md"
+            />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+
+        <q-item
+          clickable
+          v-ripple
+          to="/about"
+        >
+          <q-item-section avatar>
+            <q-icon
+              color="green-4"
+              name="help"
+              size="md"
+            />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+        </q-item>
+
+      </q-list>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
