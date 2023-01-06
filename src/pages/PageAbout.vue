b53afd565d91cf776a39c30dfa6726614dd08288
lapinRagnar <lapinragnar@gmail.com>
2023-01-06 21:12:01 +0100

    5 - html du page about

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,7 +1,13 @@
 <template>
-  <q-page>
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About twitter</h4>
+
+    <div class="text-body1">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, omnis quae assumenda fuga voluptas amet magnam porro quaerat officia consectetur id aliquid nemo blanditiis ducimus veniam vitae itaque, in inventore.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore, eum, quod illum fugiat quaerat quo, fugit nulla velit modi nihil. Temporibus, atque optio? Quia deleniti optio eaque eos eius.</p>
+    </div>
   </q-page>
+  
 </template>
 
 <script>
