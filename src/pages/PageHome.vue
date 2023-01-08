f4da2d20bbc03161d89d5d2fd9c4ad89a75f2561
lapinRagnar <lapinragnar@gmail.com>
2023-01-08 20:29:19 +0100

    home - tweet lists

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -45,6 +45,75 @@
       size="10px"
       color="grey-4"
     />
+
+    <q-list>
+
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1">
+            <strong>lapinRagnar</strong>
+            <span class="text-grey-7"> @lapinRagnar</span>
+          </q-item-label>
+          <q-item-label class="qweet-content text-body1">
+            <span class="text-weight-bold">Janet</span>
+            -- I'll be in your neighborhood doing errands this
+            weekend. Do you want to grab brunch?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+            Laboriosam eos provident enim quod molestias dolore perferendis consequuntur obcaecati quos alias, velit veritatis perspiciatis at aliquam suscipit nesciunt quidem tempora ad!
+          </q-item-label>
+
+          <div class="row justify-between q-mt-sm qweet-icons">
+            <q-btn
+              flat
+              round
+              color="grey"
+              icon="fa-regular fa-comment"
+              size="sm"
+            />
+
+            <q-btn
+              flat
+              round
+              color="grey"
+              icon="fas fa-retweet"
+              size="sm"
+            />
+
+            <q-btn
+              flat
+              round
+              color="grey"
+              icon="far fa-heart"
+              size="sm"
+            />
+            <q-btn
+              flat
+              round
+              color="grey"
+              icon="fas fa-trash"
+              size="sm"
+            />
+
+
+
+          </div>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+
+    </q-list>
+
+
   </q-page>
 </template>
 
@@ -71,5 +140,13 @@ export default defineComponent({
     border-top: 1px solid;
     border-bottom: 1px solid;
     border-color: $orange-2;
+  }
+
+  .qweet-content {
+    white-space: pre-line;
+  }
+
+  .qweet-icons {
+    margin-left: -5px;
   }
 </style>
