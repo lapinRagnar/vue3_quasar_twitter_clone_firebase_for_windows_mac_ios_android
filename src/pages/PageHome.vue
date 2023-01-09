d523864e5405c8f4420bc3c20df37dd6f1f7bded
lapinRagnar <lapinragnar@gmail.com>
2023-01-09 20:10:21 +0100

    9 - ajout un scroll dans la page home

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,135 +1,132 @@
 <template>
 
-  <q-page>
+  <q-page class="relative-position">
 
-    <div class="q-py-lg q-px-md row items-end q-col-gutter-lg">
+    <q-scroll-area class="absolute fullscreen">
 
-      <div class="col">
+      <div class="q-py-lg q-px-md row items-end q-col-gutter-lg">
 
-        <q-input
-          class="new-tweet"
-          bottom-slots
-          v-model="newTweetContent"
-          placeholder="What's happining ?"
-          counter
-          maxlength="300"
-          autogrow
-        >
-          <template v-slot:before>
-            <q-avatar size="xl">
-              <img src="https://cdn.quasar.dev/img/avatar5.jpg">
-            </q-avatar>
-          </template>
+        <div class="col">
+
+          <q-input
+            class="new-tweet"
+            bottom-slots
+            v-model="newTweetContent"
+            placeholder="What's happining ?"
+            counter
+            maxlength="300"
+            autogrow
+          >
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+              </q-avatar>
+            </template>
 
 
-        </q-input>
+          </q-input>
+
+        </div>
+
+        <div class="col col-shrink">
+          <q-btn
+            @click="addNewQweet"
+            class="q-mb-lg"
+            unelevated
+            rounded
+            color="orange"
+            label="tweet"
+            no-caps
+            :disabled="!newTweetContent"
+          />
+        </div>
+
 
       </div>
 
-      <div class="col col-shrink">
-        <q-btn
-          @click="addNewQweet"
-          class="q-mb-lg"
-          unelevated
-          rounded
-          color="orange"
-          label="tweet"
-          no-caps
-          :disabled="!newTweetContent"
-        />
-      </div>
+      <q-separator
+        class="divider"
+        size="10px"
+        color="grey-4"
+      />
 
+      <q-list separator>
 
-    </div>
-
-    <q-separator
-      class="divider"
-      size="10px"
-      color="grey-4"
-    />
-
-
-
-    <q-list separator>
-
-      <transition-group
-        appear
-        enter-active-class="animated zoomInUp slower"
-        leave-active-class="animated zoomOutDown slower"
-      >
-        <q-item
-        class="q-py-md"
-        v-for="(qweet, i) in qweets"
-        :key="i"
+        <transition-group
+          appear
+          enter-active-class="animated zoomInUp slower"
+          leave-active-class="animated zoomOutDown slower"
         >
-          <q-item-section avatar top>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/img/avatar2.jpg">
-            </q-avatar>
-          </q-item-section>
+          <q-item
+          class="q-py-md"
+          v-for="(qweet, i) in qweets"
+          :key="i"
+          >
+            <q-item-section avatar top>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+              </q-avatar>
+            </q-item-section>
 
-          <q-item-section>
-            <q-item-label class="text-subtitle1">
-              <strong>lapinRagnar</strong>
-              <span class="text-grey-7"> @lapinRagnar</span>
-            </q-item-label>
-            <q-item-label class="qweet-content text-body1">
-              <span class="text-weight-bold">Janet</span>
-              {{ qweet.content }}
-            </q-item-label>
+            <q-item-section>
+              <q-item-label class="text-subtitle1">
+                <strong>lapinRagnar</strong>
+                <span class="text-grey-7"> @lapinRagnar <br class="lt-md"> - {{ qweet.date }}</span>
+              </q-item-label>
+              <q-item-label class="qweet-content text-body1">
+                <span class="text-weight-bold">Janet</span>
+                {{ qweet.content }}
+              </q-item-label>
 
-            <div class="row justify-between q-mt-sm qweet-icons">
+              <div class="row justify-between q-mt-sm qweet-icons">
 
-              <q-btn
-                flat
-                round
-                color="grey"
-                icon="fa-regular fa-comment"
-                size="sm"
-              />
+                <q-btn
+                  flat
+                  round
+                  color="grey"
+                  icon="fa-regular fa-comment"
+                  size="sm"
+                />
 
-              <q-btn
-                flat
-                round
-                color="grey"
-                icon="fas fa-retweet"
-                size="sm"
-              />
+                <q-btn
+                  flat
+                  round
+                  color="grey"
+                  icon="fas fa-retweet"
+                  size="sm"
+                />
 
-              <q-btn
-                flat
-                round
-                color="grey"
-                icon="far fa-heart"
-                size="sm"
-              />
-              <q-btn
-                @click="deleteQtweet(qweet)"
-                flat
-                round
-                color="grey"
-                icon="fas fa-trash"
-                size="sm"
-              />
-
-
-            </div>
-
-          </q-item-section>
-
-          <q-item-section side top>
-            {{ qweet.date }}
-          </q-item-section>
-
-        </q-item>
-
-      </transition-group>
-
-    </q-list>
+                <q-btn
+                  flat
+                  round
+                  color="grey"
+                  icon="far fa-heart"
+                  size="sm"
+                />
+                <q-btn
+                  @click="deleteQtweet(qweet)"
+                  flat
+                  round
+                  color="grey"
+                  icon="fas fa-trash"
+                  size="sm"
+                />
 
 
+              </div>
+
+            </q-item-section>
+
+          </q-item>
+
+        </transition-group>
+
+      </q-list>
+
+    </q-scroll-area>
 
   </q-page>
+
 </template>
 
 <script>
